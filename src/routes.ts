f1f33facfca9d3cc92d7510a1cd95f4141cbda40
Leonardo Savério <leonardosaverio@hotmail.com>
2022-05-17 22:56:09 -0300

    autenticação, criação de usuario, e middlaware para validar usuario autenticado

--- src/routes.ts
+++ src/routes.ts
@@ -2,16 +2,17 @@ import { Router, Request, Response, response } from 'express';
 import multer from "multer";
 import multerConfig from './config/multer'; 
 
+import authMiddleware from './middlewares/authMiddleware';
+
+import UserController from './controllers/UserController';
+import AuthController from './controllers/AuthController';
 
 const router = Router();
 const upload = multer(multerConfig);
 
 
-router.get('/receitas', (req: Request, res: Response)=> {
-    return res.send({
-        hello: 'hello world'
-    })
-} );
+router.post('/users', UserController.create);
+router.post('/auth', AuthController.authenticate);
 
 
 export default router;
