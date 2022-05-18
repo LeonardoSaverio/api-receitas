0d11701a1ade23cb802a4e245a03ff42779fc22a
Leonardo Savério <leonardosaverio@hotmail.com>
2022-05-17 22:39:05 -0300

    serve default error

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import "reflect-metadata";
 import 'express-async-errors';
 dotenv.config();
 import routes from './routes';
-// import errorHandler from './errors/handler'
+import errorHandler from './errors/handler'
 
 import './database/connection';
 
@@ -16,6 +16,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(routes);
-// app.use(errorHandler);
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 3333, () => console.log('🔥️ Server started at http://localhost/3333'));
